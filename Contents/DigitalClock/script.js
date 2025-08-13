4486d7eeceae705452ff0584ce8850357811c84d
Rajeshwari1403 <rajeshwariravi2005@gmail.com>
2025-08-13 19:06:48 +0530

    js

--- Contents/DigitalClock/script.js
+++ Contents/DigitalClock/script.js
@@ -4,3 +4,26 @@ icons = document.querySelector(".icons")
 icons.onclick = () => {
   section.classList.toggle("dark");
 }
+
+// Creating Function for every Second
+setInterval(() => {
+  let date = new Date(),
+  hour = date.getHours(),
+  min = date.getMinutes(),
+  sec = date.getSeconds();
+
+  let d;
+  d = hour < 12 ? "AM" : "PM";
+  hour = hour > 12 ? hour - 12 : hour;
+  hour = hour == 0 ? hour = 12 : hour;
+
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+  console.log(date);
+
+  document.querySelector(".hour").innerText = hour;
+  document.querySelector(".min").innerText = min;
+  document.querySelector(".sec").innerText = sec;
+  document.querySelector(".am_pm").innerText = d;
+}, 1000); // 1000 milliseconds = 1s
